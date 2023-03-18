7a4d806536192a9c08bd195ca06d9968800f7896
blessja <jaydenj772@gmail.com>
2023-03-18 23:06:10 +0200

    Added REST API V1

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const colors = require("colors");
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
-// const connectDB = require('./config/db')
+const connectDB = require('./config/db')
 
 // connect to database
+connectDB()
+
+// console.log(process.env.PORT)
+// console.log(process.env.NODE_ENV)
+
+// console.log(process.env.MONGO_URI)
+
 const port = process.env.PORT  || 5000
 
 const app = express()
