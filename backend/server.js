d83bfcbf923a2f4bfafd11f43a18329867554f59
blessja <jaydenj772@gmail.com>
2023-03-18 21:48:41 +0200

    controller goals

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,23 @@
-console.log("hello jack")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+// const connectDB = require('./config/db')
+
+// connect to database
+const port = process.env.PORT  || 5000
+
+const app = express()
+
+// adding middleware
+
+app.use(express.json())
+
+app.use(express.urlencoded({extended: false}))
+
+app.use(errorHandler)
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+
+
+app.listen(port, () => console.log(`Port is running ${port}`))
